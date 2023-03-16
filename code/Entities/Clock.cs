b16429ac93d3b2fd67425eb5f133648b87547499
Daniel From Kjærgaard <KongzCringe@gmail.com>
2023-03-16 19:39:01 +0100

    q

--- code/Entities/Clock.cs
+++ code/Entities/Clock.cs
@@ -1,35 +1,19 @@
-﻿using Editor;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Sandbox;
+using Editor;
 
 namespace Sandbox.Entities
 {
 
-	using Sandbox;
-	using Editor;
+	
 
 	/// <summary>
 	/// This entity defines the spawn point of the player in first person shooter gamemodes.
 	/// </summary>
-	[Library( "info_player_start" ), HammerEntity]
-	[Title( "Player Spawnpoint" ), Category( "Player" ), Icon( "place" )]
+	[Library( "ent_Clock" ), HammerEntity]
+	[Title( "Clock" ), Category( "Player" ), Icon( "place" )]
 	[Model]
-	public class SpawnPoint : Entity, IUse
+	public class ClockEntity : Entity
 	{
-		public bool IsUsable( Entity user )
-		{
-			if ( Vector3.DistanceBetween( user.Position, Position ) < 5 ) return true;
-
-			return false;
-		}
-
-		public bool OnUse( Entity user )
-		{
-			Log.Info( "Virker" );
-			return false;
-		}
+		
 	}
 }
