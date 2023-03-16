5cc3c16b86f67d6fe9f676347538dc88ad7bc98a
Daniel From Kjærgaard <KongzCringe@gmail.com>
2023-03-16 19:45:42 +0100

    q

--- code/Entities/Clock.cs
+++ code/Entities/Clock.cs
@@ -10,10 +10,11 @@ namespace Sandbox.Entities
 	/// This entity defines the spawn point of the player in first person shooter gamemodes.
 	/// </summary>
 	[Library( "ent_Clock" ), HammerEntity]
-	[Title( "Clock" ), Category( "Player" ), Icon( "place" )]
+	[Title( "ClockEntity" ), Category( "Placeable" ), Icon( "place" )]
 	[Model]
 	public class ClockEntity : Entity
 	{
-		
+		[Property( Title = "Start Disabled" )]
+		public bool StartDisabled { get; set; } = false;
 	}
 }
