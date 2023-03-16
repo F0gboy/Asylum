69b3b274e63183404045ca4d9ea27ebc354ab7ed
Daniel From Kjærgaard <KongzCringe@gmail.com>
2023-03-16 20:28:19 +0100

    w

--- code/Entities/Clock.cs
+++ code/Entities/Clock.cs
@@ -5,12 +5,12 @@ using Editor;
 /// This entity defines the spawn point of the player in first person shooter gamemodes.
 /// </summary>
 
-//[Library( "ent_clock" )]
+[Library( "ent_clock" )]
 [HammerEntity, SupportsSolid]
 [RenderFields, VisGroup( VisGroup.Dynamic )]
 [Title( "Clock" ), Category( "Placeable" ), Icon( "radio_button_checked" )]
 [Model( Archetypes = ModelArchetype.animated_model | ModelArchetype.static_prop_model )]
-public class ClockEntity : KeyframeEntity, IUse
+public partial class ClockEntity : KeyframeEntity, IUse
 {
 	public bool IsUsable( Entity user )
 	{
