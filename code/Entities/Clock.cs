490d7e867842933a176fc258d580aa5705accb41
Daniel From Kjærgaard <KongzCringe@gmail.com>
2023-03-16 20:21:48 +0100

    e

--- code/Entities/Clock.cs
+++ code/Entities/Clock.cs
@@ -1,29 +1,26 @@
 ﻿using Sandbox;
 using Editor;
 
-namespace Sandbox.Entities
+/// <summary>
+/// This entity defines the spawn point of the player in first person shooter gamemodes.
+/// </summary>
+
+[Library( "ent_clock" )]
+[HammerEntity, SupportsSolid]
+[RenderFields, VisGroup( VisGroup.Dynamic )]
+[Title( "Clock" ), Category( "Placeable" ), Icon( "radio_button_checked" )]
+[Model( Archetypes = ModelArchetype.animated_model | ModelArchetype.static_prop_model )]
+public class ClockEntity : KeyframeEntity, IUse
 {
-	/// <summary>
-	/// This entity defines the spawn point of the player in first person shooter gamemodes.
-	/// </summary>
-
-	[Library( "ent_clock" )]
-	[HammerEntity, SupportsSolid]
-	[RenderFields, VisGroup( VisGroup.Dynamic )]
-	[Title( "Clock" ), Category( "Placeable" ), Icon( "radio_button_checked" )]
-	[Model( Archetypes = ModelArchetype.animated_model | ModelArchetype.static_prop_model )]
-	public class ClockEntity : KeyframeEntity, IUse
+	public bool IsUsable( Entity user )
 	{
-		public bool IsUsable( Entity user )
-		{
-			return true;
-		}
+		return true;
+	}
 
-		public bool OnUse( Entity user )
-		{
-			Log.Info( "Wtf" );
+	public bool OnUse( Entity user )
+	{
+		Log.Info( "Wtf" );
 
-			return true;
-		}
+		return true;
 	}
 }
