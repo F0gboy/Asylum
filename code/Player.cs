8ab60813ad69d38bac3b3e65b11fc52ebfb2ed44
Daniel From Kjærgaard <KongzCringe@gmail.com>
2023-03-16 13:54:44 +0100

    start

---
+++ code/Player.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Sandbox.Event;
+
+namespace Sandbox
+{
+	internal class MyPlayer : Player
+	{
+		public ClothingContainer Clothing = new();
+
+		public MyPlayer() : base()
+		{
+			Inventory = new Inventory( this );
+		}
+
+		public MyPlayer( IClient client ) : this()
+		{
+			SetModel( "models/citizen/citizen.vmdl" );
+			Clothing.LoadFromClient(client);
+		}
+
+		public override void Respawn()
+		{
+			Log.Info( "Spawner spiller" );
+			Clothing.DressEntity( this );
+
+			Controller = new WalkController();
+
+			EnableAllCollisions = true;
+			EnableDrawing = true;
+			EnableHideInFirstPerson = true;
+			EnableShadowInFirstPerson = true;
+
+			Inventory.Add( new Flashlight(), true );
+
+			base.Respawn();
+		}
+
+		public override PawnController GetActiveController()
+		{
+			if ( DevController != null ) return DevController;
+
+			return base.GetActiveController();
+		}
+
+		public override void Simulate( IClient client )
+		{
+			base.Simulate( client );
+
+			var controller = GetActiveController();
+			if ( controller != null )
+			{
+				EnableSolidCollisions = !controller.HasTag( "noclip" );
+
+				SimulateAnimation( controller );
+			}
+		}
+
+		Entity lastWeapon;
+
+		void SimulateAnimation( PawnController controller )
+		{
+			if ( controller == null )
+				return;
+
+			// where should we be rotated to
+			var turnSpeed = 0.02f;
+
+			Rotation rotation;
+
+			// If we're a bot, spin us around 180 degrees.
+			if ( Client.IsBot )
+				rotation = ViewAngles.WithYaw( ViewAngles.yaw + 180f ).ToRotation();
+			else
+				rotation = ViewAngles.ToRotation();
+
+			var idealRotation = Rotation.LookAt( rotation.Forward.WithZ( 0 ), Vector3.Up );
+			Rotation = Rotation.Slerp( Rotation, idealRotation, controller.WishVelocity.Length * Time.Delta * turnSpeed );
+			Rotation = Rotation.Clamp( idealRotation, 45.0f, out var shuffle ); // lock facing to within 45 degrees of look direction
+
+			CitizenAnimationHelper animHelper = new CitizenAnimationHelper( this );
+
+			animHelper.WithWishVelocity( controller.WishVelocity );
+			animHelper.WithVelocity( controller.Velocity );
+			animHelper.WithLookAt( EyePosition + EyeRotation.Forward * 100.0f, 1.0f, 1.0f, 0.5f );
+			animHelper.AimAngle = rotation;
+			animHelper.FootShuffle = shuffle;
+			animHelper.DuckLevel = MathX.Lerp( animHelper.DuckLevel, controller.HasTag( "ducked" ) ? 1 : 0, Time.Delta * 10.0f );
+			animHelper.VoiceLevel = (Game.IsClient && Client.IsValid()) ? Client.Voice.LastHeard < 0.5f ? Client.Voice.CurrentLevel : 0.0f : 0.0f;
+			animHelper.IsGrounded = GroundEntity != null;
+			animHelper.IsSitting = controller.HasTag( "sitting" );
+			animHelper.IsNoclipping = controller.HasTag( "noclip" );
+			animHelper.IsClimbing = controller.HasTag( "climbing" );
+			animHelper.IsSwimming = this.GetWaterLevel() >= 0.5f;
+			animHelper.IsWeaponLowered = false;
+
+			if ( controller.HasEvent( "jump" ) ) animHelper.TriggerJump();
+			if ( ActiveChild != lastWeapon ) animHelper.TriggerDeploy();
+
+			if ( ActiveChild is BaseCarriable carry )
+			{
+				carry.SimulateAnimator( animHelper );
+			}
+			else
+			{
+				animHelper.HoldType = CitizenAnimationHelper.HoldTypes.None;
+				animHelper.AimBodyWeight = 0.5f;
+			}
+
+			lastWeapon = ActiveChild;
+		}
+	}
+}
---
+++ code/tools/Flashlight.cs
@@ -0,0 +1,147 @@
+﻿using Sandbox;
+
+[Spawnable]
+[Library( "weapon_flashlight", Title = "Flashlight" )]
+partial class Flashlight : BaseWeapon
+{
+	public override string ViewModelPath => "weapons/rust_flashlight/v_rust_flashlight.vmdl";
+	public override float SecondaryRate => 2.0f;
+
+	protected virtual Vector3 LightOffset => Vector3.Forward * 10;
+
+	private SpotLightEntity worldLight;
+	private SpotLightEntity viewLight;
+
+	[Net, Local, Predicted]
+	private bool LightEnabled { get; set; } = true;
+
+	TimeSince timeSinceLightToggled;
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		SetModel( "weapons/rust_pistol/rust_pistol.vmdl" );
+
+		worldLight = CreateLight();
+		worldLight.SetParent( this, "slide", new Transform( LightOffset ) );
+		worldLight.EnableHideInFirstPerson = true;
+		worldLight.Enabled = false;
+	}
+
+	public override void CreateViewModel()
+	{
+		base.CreateViewModel();
+
+		viewLight = CreateLight();
+		viewLight.SetParent( ViewModelEntity, "light", new Transform( LightOffset ) );
+		viewLight.EnableViewmodelRendering = true;
+		viewLight.Enabled = LightEnabled;
+	}
+
+	private SpotLightEntity CreateLight()
+	{
+		var light = new SpotLightEntity
+		{
+			Enabled = true,
+			DynamicShadows = true,
+			Range = 512,
+			Falloff = 1.0f,
+			LinearAttenuation = 0.0f,
+			QuadraticAttenuation = 1.0f,
+			Brightness = 2,
+			Color = Color.White,
+			InnerConeAngle = 20,
+			OuterConeAngle = 40,
+			FogStrength = 1.0f,
+			Owner = Owner,
+			LightCookie = Texture.Load( "materials/effects/lightcookie.vtex" )
+		};
+
+		return light;
+	}
+
+	public override void Simulate( IClient cl )
+	{
+		if ( cl == null )
+			return;
+
+		base.Simulate( cl );
+
+		bool toggle = Input.Pressed( InputButton.Flashlight ) || Input.Pressed( InputButton.PrimaryAttack );
+
+		if ( timeSinceLightToggled > 0.1f && toggle )
+		{
+			LightEnabled = !LightEnabled;
+
+			PlaySound( LightEnabled ? "flashlight-on" : "flashlight-off" );
+
+			if ( worldLight.IsValid() )
+			{
+				worldLight.Enabled = LightEnabled;
+			}
+
+			if ( viewLight.IsValid() )
+			{
+				viewLight.Enabled = LightEnabled;
+			}
+
+			timeSinceLightToggled = 0;
+		}
+	}
+
+	public override bool CanReload()
+	{
+		return false;
+	}
+
+	private void Activate()
+	{
+		if ( worldLight.IsValid() )
+		{
+			worldLight.Enabled = LightEnabled;
+		}
+	}
+
+	private void Deactivate()
+	{
+		if ( worldLight.IsValid() )
+		{
+			worldLight.Enabled = false;
+		}
+	}
+
+	public override void ActiveStart( Entity ent )
+	{
+		base.ActiveStart( ent );
+
+		if ( Game.IsServer )
+		{
+			Activate();
+		}
+	}
+
+	public override void ActiveEnd( Entity ent, bool dropped )
+	{
+		base.ActiveEnd( ent, dropped );
+
+		if ( Game.IsServer )
+		{
+			if ( dropped )
+			{
+				Activate();
+			}
+			else
+			{
+				Deactivate();
+			}
+		}
+	}
+
+	public override void SimulateAnimator( CitizenAnimationHelper anim )
+	{
+		anim.HoldType = CitizenAnimationHelper.HoldTypes.Pistol;
+		anim.Handedness = CitizenAnimationHelper.Hand.Right;
+		anim.AimBodyWeight = 1.0f;
+	}
+}
