8ee374835002237c6abf5de0d2fee0409c88f9a6
Darkedit <ae@haah.dk>
2023-03-23 14:12:33 +0100

    Clock

--- code/Game.cs
+++ code/Game.cs
@@ -26,8 +26,9 @@ partial class MyGame : GameManager
 		if ( Game.IsClient )
 		{
 			_ = new PlayersReady();
-			_ = new BellUI();
 
+			_ = new BellUI();
+			
 			var text = $"Players ready: {GetPlayersReady()}/{Game.Clients.Count}";
 			UpdatePlayerReadyText( To.Single( Game.LocalPawn as MyPlayer), text, true, allPlayersReady);
 		}
